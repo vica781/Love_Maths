b87b9cd884be6245e03ca0487513cc1ac18d9ae0
Victoria Kalenik <vk781@hotmail.com>
2023-03-02 18:40:55 +0000

    Add event listeners for document loading and button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,69 @@
-/**
- * The main gaim "loop", called when the script is first loaded
- * and after the user's answer has been processed
- */
+//Wait for the DOM to finish loading before running the game
+//Getr the button elements and add event listeners to them 
 
-function runGame() {
-    //Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByClassName("button");
 
-function checkAnswer() {
-
-}
-
-/**
- * Gets the operands (the numbers) and the operator (plus, minus, etc.)
- * directly from the DOM, and returns the correct answer
- */
-function calculateCorrectAnswer() {
-    
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById('operator').innerText;
-
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-        
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttrubute("data-type") === "submit") {
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttrubute("data-type");
+                alert(`You click ${gameType}`);
+            }
+        })
     }
-}
+})
 
-function incrementScore() {
 
-}
+// /**
+//  * The main gaim "loop", called when the script is first loaded
+//  * and after the user's answer has been processed
+//  */
 
-function incrementWrongAnswer() {
+// function runGame() {
+//     //Creates two random numbers between 1 and 25
+//     let num1 = Math.floor(Math.random() * 25) + 1;
+//     let num2 = Math.floor(Math.random() * 25) + 1;
+// }
 
-}
+// function checkAnswer() {
 
-function displayAdditionQuestion() {
+// }
 
-}
+// /**
+//  * Gets the operands (the numbers) and the operator (plus, minus, etc.)
+//  * directly from the DOM, and returns the correct answer
+//  */
+// function calculateCorrectAnswer() {
+    
+//     let operand1 = parseInt(document.getElementById('operand1').innerText);
+//     let operand2 = parseInt(document.getElementById('operand2').innerText);
+//     let operator = document.getElementById('operator').innerText;
 
-function displaySubtractQuestion() {
+//     if (operator === "+") {
+//         return [operand1 + operand2, "addition"];
+        
+//     }
+// }
 
-}
+// function incrementScore() {
 
-function displayMultiplyQuestion() {
+// }
 
-}
+// function incrementWrongAnswer() {
+
+// }
+
+// function displayAdditionQuestion() {
+
+// }
+
+// function displaySubtractQuestion() {
+
+// }
+
+// function displayMultiplyQuestion() {
+
+// }
